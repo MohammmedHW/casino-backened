708f3cc675cfbfb0d6554891bee62b5d5ce720da
Mohammad Khachrod Wala <mohammed53saifee@gmail.com>
2025-04-22 21:45:25 +0530

    add Blog controller with CRUD operations and update server configuration for routing

---
+++ server/controllers/Blog.Controller.js
@@ -0,0 +1,63 @@
+const Blog = require("../models/Blog.Model");
+
+// Get all blogs
+exports.getAllBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find().sort({ publishDate: -1 });
+    res.json(blogs);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Get single blog
+exports.getBlogById = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.json(blog);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Create blog
+exports.createBlog = async (req, res) => {
+  try {
+    const blog = new Blog(req.body);
+    const savedBlog = await blog.save();
+    res.status(201).json(savedBlog);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Update blog
+exports.updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.json(blog);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete blog
+exports.deleteBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndDelete(req.params.id);
+    if (!blog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.json({ message: "Blog removed" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,30 +1,29 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-const database = require("./config/database.config");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const database = require("./config/database.config");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
 // const { cloudinaryConnect } = require("./config/cloudinary");
 // const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
 
-dotenv.config();
-const PORT = process.env.PORT || 4000;
+// dotenv.config();
+// const PORT = process.env.PORT || 4000;
 
-//database connect
-database.connect();
-//middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //for using postman
-app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    // origin: "https://seccouncil.com",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// //database connect
+// database.connect();
+// //middlewares
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true })); //for using postman
+// app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 
 // app.use(
 //   fileUpload({
@@ -32,20 +31,83 @@ app.use(
 //     tempFileDir: "/tmp",
 //   })
 // );
-//cloudinary connection
+// // cloudinary connection
 // cloudinaryConnect();
 
-//routes
+// //routes
 
-//def route
+// //def route
 
+// app.get("/", (req, res) => {
+//   return res.json({
+//     success: true,
+//     message: "Your server is up and running....",
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`App is running at ${PORT}`);
+// });
+
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+// Config
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+// Import configs
+const database = require("./config/database.config");
+const { cloudinaryConnect } = require("./config/cloudinary");
+
+// Import Routes
+const casinoRoutes = require("./routes/Casino.Routes");
+const blogRoutes = require("./routes/Blogs.Routes");
+const authRoutes = require("./routes/Auth.Routes");
+
+// Database connect
+database.connect();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+// Cloudinary connection
+cloudinaryConnect();
+
+// Routes
+app.use("/api/casinos", casinoRoutes);
+app.use("/api/blogs", blogRoutes);
+app.use("/api/auth", authRoutes);
+
+// Default route
 app.get("/", (req, res) => {
   return res.json({
     success: true,
-    message: "Your server is up and running....",
+    message: "Casino Website API is running...",
   });
 });
 
+// Start server
 app.listen(PORT, () => {
-  console.log(`App is running at ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
